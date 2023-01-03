36a865509e383deb65847efc55968021dc8fb9b3
CatStorm-dev <wapitiblase36@gmail.com>
2023-01-03 16:04:15 +0100

    tried to implement the font

--- src/App.java
+++ src/App.java
@@ -3,8 +3,10 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
 
+//This class contains the main frame
 public class App
 {
     JFrame f = new JFrame();
@@ -13,15 +15,17 @@ public class App
     Image img;
     Stardate stardate = new Stardate();
 
-    public void main()
+    public void main()throws Exception
     {
+        URL fontURL = new URL("https://github.com/CatStorm1208/lcars/blob/4350329c5b3c976a0fec64c79b3572582ee312a8/src/Resources/LCARSGTJ3.ttf");
+        Font lcarsFont = Font.createFont(Font.TRUETYPE_FONT, fontURL.openStream());
         f.setExtendedState(JFrame.MAXIMIZED_BOTH);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.getContentPane().setBackground(Color.black);
         f.setLayout(null);
         f.setVisible(true);
         sussyButton();
-        stardateDisplay();
+        stardateDisplay(lcarsFont);
         f.add(starDateText);
     }
 
@@ -42,10 +46,12 @@ public class App
         }
     }
 
-    void stardateDisplay()
+    void stardateDisplay(Font _f)
     {
-        //TODO implement font
+        //TODO make the font actually work
         //TODO copyright symbol for used font
+        starDateText.setFont(_f);
+        starDateText.setSize(100,100);
         starDateText.setText(Integer.toString(stardate.stardate()));
         starDateText.setBorder(BorderFactory.createEmptyBorder());
         starDateText.setBackground(Color.black);
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main
 {
     static App app = new App();
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
         app.main();
     }
